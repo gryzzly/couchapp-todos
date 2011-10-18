1e0cd60e15fe741e3477bdbf642e53114d218d63
Misha Reyzlin <hello@mishareyzlin.com>
2011-10-18 11:54:00 +0700

    documenting update / show handlers

--- shows/todos.js
+++ shows/todos.js
@@ -1,3 +1,11 @@
+// http://wiki.apache.org/couchdb/Formatting_with_Show_and_List
+// 
+// Show function – when queryied with GET returns a representation of a document.
+//
+// @param {Object} - doc - populated with the document, if the update function
+// was requested with a document id. 
+// @param {Object} - req - populated with information about the request environment.
+// @return {Object} – response object, may contain string (as body), or hashes for header and body.
 function ( doc, req ) { 
   return {
     headers : { 
--- updates/todos.js
+++ updates/todos.js
@@ -1,7 +1,16 @@
+// http://wiki.apache.org/couchdb/Document_Update_Handlers
+//
+// Update function – invokes server-side logic (augmenting documents in db) – accepting POST and PUT
+// 
+// @param {Object} - doc - populated with the document, if the update function
+// was requested with a document id. 
+// @param {Object} - req - populated with information about the request environment.
+// @return {Array} – the first element is the (updated or new) document, which is committed to the database. If the first element is `null` no document will be committed to the database. If you are updating an existing, it should already have an _id set, and if you are creating a new document, make sure to set its _id to something, either generated based on the input or the req.uuid provided. The second element is the response that will be sent back to the caller.
+//
 function ( doc, req ) {
   return [{
     _id : req.id,
-    title : req.id,
+    title : req.body,
     isDone : false
   },
     JSON.stringify( req )
